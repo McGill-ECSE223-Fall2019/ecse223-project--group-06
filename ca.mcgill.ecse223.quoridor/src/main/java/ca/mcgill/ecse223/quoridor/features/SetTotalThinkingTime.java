f34b8e38e047f1e30e9860a2436c3abbdedfd447
xiangyu li <xiangyu.li3@mail.mcgill.ca>
2019-10-08 12:39:32 -0700

    change

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/SetTotalThinkingTime.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/SetTotalThinkingTime.java
@@ -1,5 +1,15 @@
 package ca.mcgill.ecse223.quoridor.features;
 
-public class SetTotalThinkingTime {
 
+import com.sun.glass.ui.Timer;
+
+public class SetTotalThinkingTime {
+	Timer timer;
+	public SetTotalThinkingTime() {
+		
+	}
+	public static void setTime(long hours, long mintues, long seconds) {
+		
+		
+	}
 }
