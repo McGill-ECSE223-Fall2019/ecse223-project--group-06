337f397512ee1a99e147b491c9124b09740b9e67
xiangyu li <xiangyu.li3@mail.mcgill.ca>
2019-10-08 15:11:15 -0700

    add javadoc

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/SetTotalThinkingTime.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/SetTotalThinkingTime.java
@@ -1,15 +1,32 @@
 package ca.mcgill.ecse223.quoridor.features;
-
-
-import com.sun.glass.ui.Timer;
+/**
+* SetTotalThinkingTime is used to set think time for each player as long as the game starts
+* 
+* @author Xiangyu Li
+* 
+*/
+import java.sql.Time;
+import ca.mcgill.ecse223.quoridor.model.Player;
 
 public class SetTotalThinkingTime {
-	Timer timer;
+	
 	public SetTotalThinkingTime() {
-		
 	}
-	public static void setTime(long hours, long mintues, long seconds) {
-		
-		
+/**
+ * This method is used for setting total thinking time for each players
+ * @param totaltimet total thinking time for each player, input from UI 
+ * @param player1, player2 players currently play this game 
+ * 
+ */
+	public static void setTimefor2(Time totaltime, Player player1, Player player2) {
+		player1.setRemainingTime(totaltime);
+		player2.setRemainingTime(totaltime);
 	}
+	public static void setTimefor4(Time totaltime, Player player1, Player player2, Player player3, Player player4) {
+		player1.setRemainingTime(totaltime);
+		player2.setRemainingTime(totaltime);
+		player3.setRemainingTime(totaltime);
+		player4.setRemainingTime(totaltime);
+	}
+	
 }
--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/Switchplayer.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/features/Switchplayer.java
@@ -1,5 +1,41 @@
 package ca.mcgill.ecse223.quoridor.features;
+/**
+* Switchplayer is for swap turns between two players
+* Clock for current player need to stop
+* Clock for next player need to start
+* Provides who is in this turn
+* 
+* @author Xiangyu Li
+* 
+*/
+import java.util.Timer;
+import java.util.TimerTask;
+
+import ca.mcgill.ecse223.quoridor.model.Player;
 
 public class Switchplayer {
 
+	
+	public Switchplayer() {
+		
+	}
+	/**
+	 * This method is used for swap turns and manage clock for each player 
+	 * @param current is current player's clock 
+	 * @param next is next player's clock
+	 * @param player1, player2 players currently play this game 
+	 * 
+	 */	
+	public static void makeTurn(Timer current, Timer next, Player currentplayer, Player nextplayer ) {
+		try {
+			current.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		 next.schedule(new TimerTask() {
+		      public void run() {
+		    	  }
+		   }, nextplayer.getRemainingTime());
+	}
 }
